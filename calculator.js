0bb619266ac54795e9eb1af11616a586428d40bc
fengjiao <fge.risd@gmail.com>
2024-08-08 19:46:31 -0400

    add js code to make the form funtional

--- calculator.js
+++ calculator.js
@@ -9,6 +9,22 @@ window.addEventListener('DOMContentLoaded', function () {
   }
 });
 
+// Get the inputs from the DOM.
+// Put some default values in the inputs
+// Call a function to calculate the current monthly payment
+function setupIntialValues() {
+  const values = { amount: 10000, years: 10, rate: 4.5 };
+  const amountUI = document.getElementById("loan-amount");
+  amountUI.value = values.amount;
+  const yearsUI = document.getElementById("loan-years");
+  yearsUI.value = values.years;
+  const rateUI = document.getElementById("loan-rate");
+  rateUI.value = values.rate;
+  update();
+
+}
+
+
 function getCurrentUIValues() {
   return {
     amount: +(document.getElementById("loan-amount").value),
@@ -17,26 +33,28 @@ function getCurrentUIValues() {
   }
 }
 
-// Get the inputs from the DOM.
-// Put some default values in the inputs
-// Call a function to calculate the current monthly payment
-function setupIntialValues() {
 
-}
-
-// Get the current values from the UI
-// Update the monthly payment
-function update() {
-}
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
-  const
+  const monthlyRate = (values.rate / 100) / 12;
+  const n = Math.floor(values.years * 12);
+  const monthlyPayment = (monthlyRate * values.amount) / (1 - Math.pow((1 + monthlyRate), -n)).toFixed(2)
+  return monthlyPayment
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const monthlyUser = document.getElementById("monthly-payment")
+  monthlyUser.innerHTML = '$' + monthly
+}
+
+// Get the current values from the UI
+// Update the monthly payment
+function update() {
+  const currentUserValues = getCurrentUIValues()
+  updateMonthly(calculateMonthlyPayment(currentUserValues))
 }
